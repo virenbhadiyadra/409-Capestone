a4bb1b6c64a4611fb5014ee13d6576b710cfc9af
virenbhadiyadra <122574600+virenbhadiyadra@users.noreply.github.com>
2024-10-08 23:01:03 -0700

    Create section 1
    I have created hero section in this commit.
    I have added appropriate css.
    I have added required images.
    I have used addFloating function to add floating to the images(icons).

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-// JavaScript to change icon on hover
-document.querySelectorAll('nav ul li a').forEach(item => {
-    const icon = item.querySelector('.icon');
-    const originalSrc = icon.src;
-    const hoverSrc = icon.getAttribute('data-hover');
-
-    item.addEventListener('mouseenter', () => {
-        icon.src = hoverSrc;
+// Function to add the "floating" class to specific images on the page
+const addFloating = () => {
+    // Select all images in the ".upper-imgs" section (top images)
+    const imgs = document.querySelectorAll(".upper-imgs img")
+    // Select all images in the ".bottom-imgs" section (bottom images)
+    const imgs2 = document.querySelectorAll(".bottom-imgs img")
+    // Select the image with the ".left-img" class (left-side image)
+    const imgs3 = document.querySelector(".left-img")
+    // Select the image with the ".right-img" class (right-side image)
+    const imgs4 = document.querySelector(".right-img")
+     // Add the "floating" class to all images in the top section
+    imgs.forEach(element => {
+        element.classList.add("floating")
     });
-
-    item.addEventListener('mouseleave', () => {
-        icon.src = originalSrc;
+    // Add the "floating" class to all images in the bottom section
+    imgs2.forEach(element => {
+        element.classList.add("floating")
     });
-});
+    // Add the "floating" class to the left-side image
+    imgs3.classList.add("floating")
+    // Add the "floating" class to the right-side image
+    imgs4.classList.add("floating")
+}
+// Call the addFloating function after a 1-second delay
 
+setTimeout(() => {
+    addFloating()
+}, 1000);
